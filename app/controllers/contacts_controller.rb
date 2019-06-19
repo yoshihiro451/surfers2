469ec5ba9b6170714ad75b44757ed59c33530695
yoshihiro451 <y09076739471y@gmail.com>
2019-06-19 12:15:37 +0000

    fix all

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,14 +2,19 @@ class ContactsController < ApplicationController
 	before_action :authenticate_user!
 	def new
 		@contact = Contact.new
-		@contacts = Contact.all
+		if current_user.admin
+			contacts = Contact.all
+		else
+			contacts = Contact.where(user_id: current_user.id)
+		end
+		@contacts = contacts.page(params[:page]).reverse_order
 		#@contactshow = Contact.find(params[:id])
 	end
 	def create
 		@contact = Contact.new(contact_params)
 		@contact.user_id = current_user.id
 		if @contact.save
-			redirect_to "/topics/"
+			redirect_to new_contact_path
 		else
 			render :new
 		end
@@ -27,6 +32,11 @@ class ContactsController < ApplicationController
 		@contact = Contact.find(params[:id])
 		@contact.update(contact_params)
 		redirect_to "/topics/"
+	end
+	def destroy
+		@contact = Contact.find(params[:id])
+		@contact.destroy
+		redirect_to new_contact_path
 	end
 
 	private
